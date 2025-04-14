c22686b5d7a494c191f9da318e1615816ec861ac
Carlos <charlsef23@icloud.com>
2025-04-14 23:32:11 +0200

    Initial Commit

---
+++ FitFlow/FitFlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitFlowApp.swift
+//  FitFlow
+//
+//  Created by Carlos Esteve Fernández on 14/4/25.
+//
+
+import SwiftUI
+
+@main
+struct FitFlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
