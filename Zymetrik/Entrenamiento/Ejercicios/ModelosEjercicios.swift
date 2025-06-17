b4811c37eb60ab49754f490feee7af591970d72e
Carlos <charlsef23@icloud.com>
2025-06-18 00:30:50 +0200

    Ejercicios

---
+++ Zymetrik/Entrenamiento/Ejercicios/ModelosEjercicios.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+struct Ejercicio: Identifiable, Codable {
+    let id: UUID
+    let nombre: String
+    let descripcion: String
+    let categoria: String
+    let tipo: String
+    let imagen_url: String?
+}
